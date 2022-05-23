f990c0b0f44199e6698052c9d91c2a48af12c175
Serg-101 <golikov1979@gmail.com>
2022-05-23 23:51:56 +0300

    Initial commit

---
+++ Dz1/Program.cs
@@ -0,0 +1,54 @@
+﻿/* 
+Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+
+*/
+
+int[] InitArray(int length)
+{
+    Random rnd = new Random();
+    int[] arr = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = rnd.Next(100, 1000);
+    }
+    return arr;
+}
+
+int CheckArray(int[] arr)
+{
+    int result = 0;
+    foreach (var item in arr)
+    {
+        //первый способ
+        result += 1 - item % 2;
+        //второй способ
+        if(!Convert.ToBoolean(item % 2))
+            System.Console.Write("чет ");
+        else
+            System.Console.Write("неч ");
+    }
+    System.Console.WriteLine("");
+    return result;
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (var item in arr)
+        System.Console.Write($"{item} ");
+    System.Console.WriteLine("");
+}
+
+
+Console.Clear();
+System.Console.Write("Введите размер массива ");
+int lengthArray = int.Parse(System.Console.ReadLine());
+int [] arr = InitArray(lengthArray);
+System.Console.WriteLine("");
+PrintArray(arr);
+int result = CheckArray(arr);
+System.Console.WriteLine("");
+System.Console.WriteLine($"Кол-во четных чисел в массиве {result}");
+System.Console.ReadKey();
+  
---
+++ Dz2/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+int[] InitArray(int length)
+{
+    Random rnd = new Random();
+    int[] arr = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = rnd.Next(-length, length);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (var item in arr)
+        System.Console.Write($"{item} ");
+    System.Console.WriteLine("");
+}
+
+int SummOddNumber(int[] arr)
+{
+    int result = 0;
+    for (int i = 1; i < arr.Length; i += 2)  // Прибавляем по 2 сразу
+    {
+        result += arr[i];
+    }
+    return result;
+}
+
+Console.Clear();
+System.Console.Write("Введите размер массива ");
+int lengthArray = int.Parse(System.Console.ReadLine());
+int [] arr = InitArray(lengthArray);
+System.Console.WriteLine("");
+PrintArray(arr);
+int result = SummOddNumber(arr);
+System.Console.WriteLine("");
+System.Console.WriteLine($"Сумма чисел на нечетных позициях в массиве {result}"); 
+// Счет в массиве начинается с нулевого элемента массива 
+// Ноль - четный а значит начинаем счет с элемента с индексом 1
+System.Console.ReadKey();
